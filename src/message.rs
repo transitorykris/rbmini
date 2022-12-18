158c4d750d394f1966f91f8877d5c8df50d9855f
Kris Foster <kris.foster@gmail.com>
2022-12-17 19:50:53 -0800

    Use kph instead of mms

--- src/message.rs
+++ src/message.rs
@@ -376,6 +376,11 @@ impl RbMessage {
     pub fn gps_coordinates(&self) -> Coordinates {
         self.coordinates
     }
+
+    // Returns speed in kph
+    pub fn speed(&self) -> f32 {
+        self.speed as f32 * 0.0036
+    }
 }
 
 impl fmt::Display for RbMessage {
@@ -393,7 +398,7 @@ Fix Flags        {fix_flags}
 WGS Altitude     {wgs_alt}
 MSL Altitude     {msl_alt}
 Accuracy        ({horiz_acc},\t{vert_acc})
-Speed            {speed}
+Speed            {speed:.1} kph
 Heading          {heading}
 Speed Accuracy   {speed_acc}
 Heading Accuracy {heading_acc}
@@ -416,7 +421,7 @@ Header/Checksum {header} {checksum}
             msl_alt = self.msl_altitude,
             horiz_acc = self.horizontal_accuracy,
             vert_acc = self.vertical_accuracy,
-            speed = self.speed,
+            speed = self.speed(),
             heading = self.heading,
             speed_acc = self.speed_accuracy,
             heading_acc = self.heading_accuracy,
@@ -659,5 +664,20 @@ mod tests {
         );
         assert_eq!(message.gps_coordinates().longitude(), 23.2887238);
         assert_eq!(message.gps_coordinates().latitude(), 42.6719035);
+    }
+
+    #[test]
+    fn test_speed() {
+        let raw = [
+            0xB5, 0x62, 0xFF, 0x01, 0x50, 0x00, 0xA0, 0xE7, 0x0C, 0x07, 0xE6, 0x07, 0x01, 0x0A,
+            0x08, 0x33, 0x08, 0x37, 0x19, 0x00, 0x00, 0x00, 0x2A, 0xAD, 0x4D, 0x0E, 0x03, 0x01,
+            0xEA, 0x0B, 0xC6, 0x93, 0xE1, 0x0D, 0x3B, 0x37, 0x6F, 0x19, 0x61, 0x8C, 0x09, 0x00,
+            0x0F, 0x01, 0x09, 0x00, 0x9C, 0x03, 0x00, 0x00, 0x2C, 0x07, 0x00, 0x00, 0x23, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xD0, 0x00, 0x00, 0x00, 0x88, 0xA9, 0xDD, 0x00,
+            0x2C, 0x01, 0x00, 0x59, 0xFD, 0xFF, 0x71, 0x00, 0xCE, 0x03, 0x2F, 0xFF, 0x56, 0x00,
+            0xFC, 0xFF, 0x06, 0xDB,
+        ];
+        let message = message::decode_rb_message(&raw);
+        assert_eq!(message.speed(), 0.126);
     }
 }
